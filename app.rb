bea31518dd709325e8d35f543f41028f2086c695
kazinur <kazinur@mybihs.org>
2018-08-14 17:11:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+  erb :index 
+end 
+
+get '/kazi' do 
+  erb :kazi 
+end 
+
+get '/' do 
+  erb :example 
 
 end
+
+get'/sarah' do 
+  erb :sarah 
+end 
+end 
